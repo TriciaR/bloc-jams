7ff627c252219bd7f7f8043d8627b31f04bcfdc4
TriciaR <patricia.rogers29@gmail.com>
2017-02-16 14:24:31 -0500

    Checkpoint-9-Rev0

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-  var points = document.getElementsByClassName('point');;
-
+var animatePoints = function(points) {
   var revealPoint = function(index) {
     points[index].style.opacity = 1;
     points[index].style.transform = "scaleX(1) translateY(0)";
@@ -9,7 +8,22 @@ var animatePoints = function() {
     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
   };
   for (var i = 0; i <= points.length; i++) {
-    revealPoints(i);
+    revealPoint(i);
   }
-
 };
+
+window.onload = function() {
+      // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener("scroll", function(event) {
+      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        animatePoints(pointsArray);   
+      }
+    })
+}
