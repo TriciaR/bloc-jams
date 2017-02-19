46db619aef5c4d9a214c8918ff5f390a463ad9e8
TriciaR <patricia.rogers29@gmail.com>
2017-02-18 19:50:03 -0500

    Assignment-9 Rev

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,24 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
-  var revealPoint = function(index) {
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
+var revealPoint = function(point) {
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.msTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
-  function forEach(array, callback) {
-    for (var i = 0; i < array.length; i++) {
-      callback(array[i]);
-    }
-  }
+var animatePoints = function(points) {
+  forEach(points, revealPoint);
+  // I don'tknow how to proceed with the callback here...I think it's the  revealPoints
 };
 window.onload = function() {
   if (window.innerHeight > 950) {
     animatePoints(pointsArray);
   }
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
   window.addEventListener("scroll", function(event) {
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+    if (pointsArray[0].getBoundingClientRect().top <= 500) {
       animatePoints(pointsArray);
     }
-  };)
+  });
 };
