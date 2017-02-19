c7fe1e800b53f67afc764787228fd544a9c685ac
TriciaR <patricia.rogers29@gmail.com>
2017-02-18 19:13:31 -0500

    Assignment 9 - 1st attempt - needs review

--- scripts/landing.js
+++ scripts/landing.js
@@ -7,23 +7,24 @@ var animatePoints = function(points) {
     points[index].style.msTransform = "scaleX(1) translateY(0)";
     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
   };
-  for (var i = 0; i <= points.length; i++) {
-    revealPoint(i);
+
+  function forEach(array, callback) {
+    for (var i = 0; i < array.length; i++) {
+      callback(array[i]);
+    }
   }
 };
-
 window.onload = function() {
-      // Automatically animate the points on a tall screen where scrolling can't trigger the animation
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-     }
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
 
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
-    window.addEventListener("scroll", function(event) {
-      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-        animatePoints(pointsArray);   
-      }
-    })
-}
+  window.addEventListener("scroll", function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  };)
+};
