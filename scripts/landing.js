f8a644b0118968d9924ee3c01b43f929bcd064ce
TriciaR <patricia.rogers29@gmail.com>
2017-02-28 16:59:30 -0500

    assignment-8-rev1

--- scripts/landing.js
+++ scripts/landing.js
@@ -8,8 +8,8 @@ var animatePoints = function() {
     points[index].style.msTransform = "scaleX(1) translateY(0)";
     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
   };
-  for (var i = 0; i <= points.length; i++) {
-    revealPoints(i);
+  for (var i = 0; i < points.length; i++) {
+    revealPoint(i);
   }
 
 };
