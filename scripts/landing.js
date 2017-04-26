7ebd3309bc961b36a4c1976903053e91354bb91a
TriciaR <patricia.rogers29@gmail.com>
2017-04-26 08:08:24 -0400

    checkpoint-16 Refactor LANDING with jQuery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,35 @@
-var pointsArray = document.getElementsByClassName('point');
+// #5 we remove the DOM selector (at #5) that gets the .point elements by class name. The jQuery selection of .point, added at #7, is terse enough that we don't need to store it in a variable anymore.
+var animatePoints = function() {
+  var revealPoint = function() {
+  // we use the jQuery css() method in place of the multiple style property instances. Due to jQuery's cross-browser compatibility, we don't need to use vendor prefixes on the transform property.
+  //  also the revealPoint function now refers to $(this) instead of a specific  .point element. To use this with jQuery, we must wrap it in a jQuery object.  $(this) (at #7) references a different .point element each time jQuery executes the  revealPoint() callback.
+  $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
 
-var animatePoints = function(points) {
-  var revealPoint = function(index) {
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
   };
-  for (var i = 0; i < points.length; i++) {
-    revealPoint(i);
-  }
+  // the revealPoint function no longer requires an argument. We replace the  for loop with the jQuery $.each() function. The $.each() function iterates over each .point element and executes the callback function, revealPoint.
+  $.each($('.point'), revealPoint);
 };
 
-window.onload = function() {
-      // Automatically animate the points on a tall screen where scrolling can't trigger the animation
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+$(window).load(function() {      
+  // we update the .innerHeight property to jQuery's height() method, which gets or sets an object's height. Since we pass no arguments to the function, we get the height.
+     if ($(window).height() > 950) {
+         animatePoints();
      }
 
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    // we no longer need a separate variable to hold the .selling-points element since jQuery can select the element with many fewer characters. We replace  getBoundingClientRect() with jQuery's .offset() method.
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-    window.addEventListener("scroll", function(event) {
-      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-        animatePoints(pointsArray);   
+
+
+//  the addEventListener() method becomes jQuery's scroll() method, which takes a function as an argument. jQuery's scroll() "method" is still an event handler like addEventListener(), but the jQuery wrapper obscures the appearance of events. When the window scrolls, the function executes.
+    $(window).scroll(function(event) {
+      // we replace  document.documentElement.scrollTop || document.body.scrollTop with the jQuery equivalent of $(window).scrollTop().
+        if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
+
       }
     })
-}
+});
