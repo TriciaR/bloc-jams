7042637d9116b6058cc9bd26f8efb50f3e623390
TriciaR <patricia.rogers29@gmail.com>
2017-05-03 17:55:34 -0400

    collection.js corretion png

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,7 +1,7 @@
 var buildCollectionItemTemplate = function() {
     var template =
      '<div class="collection-album-container column fourth">'
-   + '  <img src="assets/images/album_covers/0P1.png"/>'
+   + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
    + '    <p>'
    + '      <a class="album-name" href="album.html"> The Colors </a>'
